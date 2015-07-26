c280fc5c352ac646e1546df69740bbd5d1235bdf
anonymframework@gemframework.com <serif.saglam119@gmail.com>
2015-07-27 01:09:07 +0300

    first updatee

--- src/Anonym/Components/Redirect.php
+++ src/Anonym/Components/Redirect.php
@@ -8,13 +8,48 @@
      */
 
     namespace Anonym\Components\HttpClient;
-
     /**
      * Class Redirect
      * @package Anonym\Components\HttpClient
      */
     class Redirect
     {
-
-
+        /**
+         * Yönlendirme işlemi yapar
+         *
+         * @param string  $adress
+         * @param integer $time
+         */
+        public static function to($adress, $time = null)
+        {
+            if ($time === null) {
+                static::location($adress);
+            } else {
+                static::refresh($adress, $time);
+            }
+        }
+        /**
+         *  Eski sayfaya geri döndürür
+         */
+        public static function back()
+        {
+            self::to($_SERVER['HTTP_REFERER']);
+        }
+        /**
+         * YÖnlendirme yapar
+         * @param $url
+         * @param $time
+         */
+        private static function refresh($url, $time)
+        {
+            header("Refresh:{$time},url=$url");
+        }
+        /**
+         * Url yönlendirmesi yapar
+         * @param string $url
+         */
+        private static function location($url)
+        {
+            header("Location:$url");
+        }
     }
