75c2978d50be9e37e12498997d9e54869a443313
anonymframework@gemframework.com <serif.saglam119@gmail.com>
2015-07-26 23:01:58 +0300

    first update to server http headers class

--- src/Anonym/Components/ServerHttpHeaders.php
+++ src/Anonym/Components/ServerHttpHeaders.php
@@ -8,9 +8,27 @@
      */
 
     namespace Anonym\Components\HttpClient;
+    use Anonym\Components\HttpClient\RequestHeaders;
 
-
-    class ServerHttpHeaders
+    class ServerHttpHeaders extends RequestHeaders
     {
-
+        /**
+         * Server De�i�kenindeki Http headerleri atar.
+         */
+        public function __construct()
+        {
+            parent::__construct();
+            $headers = [];
+            foreach ($this->getServer() as $name => $value) {
+                if (substr($name, 0, 5) == 'HTTP_') {
+                    $name = str_replace(' ', '-', ucwords(strtolower(str_replace('_', ' ', substr($name, 5)))));
+                    $headers[$name] = $value;
+                } else if ($name == "CONTENT_TYPE") {
+                    $headers["Content-Type"] = $value;
+                } else if ($name == "CONTENT_LENGTH") {
+                    $headers["Content-Length"] = $value;
+                }
+            }
+            $this->headers = array_merge($this->headers, $headers);
+        }
     }
