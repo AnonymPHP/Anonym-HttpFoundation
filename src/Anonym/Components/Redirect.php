57bdc1998112943b53ffd74c40dc3aaa3bcce75f
anonymframework@gemframework.com <serif.saglam119@gmail.com>
2015-07-26 23:09:09 +0300

    added redirect class

---
+++ src/Anonym/Components/Redirect.php
@@ -0,0 +1,19 @@
+<?php
+    /**
+     * Bu Dosya AnonymFramework'e ait bir dosyadır.
+     *
+     * @author vahitserifsaglam <vahit.serif119@gmail.com>
+     * @see http://gemframework.com
+     *
+     */
+
+    namespace Anonym\Components\HttpClient;
+
+    /**
+     * Class Redirect
+     * @package Anonym\Components\HttpClient
+     */
+    class Redirect
+    {
+
+    }
