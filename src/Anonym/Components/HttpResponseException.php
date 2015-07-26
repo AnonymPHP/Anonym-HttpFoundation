d8c4359cc4e5b9b16753b93ef7f8b80c2d49a17e
anonymframework@gemframework.com <serif.saglam119@gmail.com>
2015-07-26 23:04:24 +0300

    added comment lines

--- src/Anonym/Components/HttpResponseException.php
+++ src/Anonym/Components/HttpResponseException.php
@@ -10,9 +10,18 @@
     namespace Anonym\Components\HttpClient;
     use Exception;
 
+    /**
+     * Class HttpResponseException
+     * @package Anonym\Components\HttpClient
+     */
     class HttpResponseException extends Exception
     {
 
+        /**
+         * Sınıfı başlatır ve istisnayı oluşturur
+         *
+         * @param string $message
+         */
         public function __construct($message = '')
         {
             $this->message = $message;
