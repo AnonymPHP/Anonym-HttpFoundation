74dbf043ae969b9e04c1f947df6c61c134f16e8e
anonymframework@gemframework.com <serif.saglam119@gmail.com>
2015-07-26 12:21:44 +0300

    updat

--- src/Anonym/Components/Response.php
+++ src/Anonym/Components/Response.php
@@ -1,87 +1,107 @@
 <?php
-
     /**
-     *  Bu Dosya AnonymFramework'un bir parçasıdır, AnonymFramework'de Yapılan isteklere gönderilecek cevabı belirler.
-
+     * Bu Dosya AnonymFramework'e ait bir dosyadır.
+     *
+     * @author vahitserifsaglam <vahit.serif119@gmail.com>
+     * @see http://gemframework.com
+     *
      */
 
     namespace Anonym\Components\HttpClient;
 
-    use Anonym\Cookie;
-    use Anonym\Components\HttpClient\Response\ShouldBeResponse;
-    use Anonym\Patterns\Singleton;
-    use Anonym\Twig;
-    use Anonym\View;
-    use Anonym\View\ShouldBeView;
-    use Anonym\Components\HttpClient\Response\HttpResponseException;
-
-    class Response implements ShouldBeResponse
+    /**
+     * Class Response
+     * @package Anonym\Components\HttpClient
+     */
+    class Response
     {
-
+        /**
+         * dosya tipi
+         *
+         * @var string
+         */
         private $contentType = 'text/html';
+
+        /**
+         * Http protocol versiyonu
+         *
+         * @var string
+         */
         private $protocolVersion;
+
+        /**
+         * Kullanılacar karekter seti
+         *
+         * @var string
+         */
         private $charset = 'utf-8';
+
+        /**
+         * Durumlara göre mesajları tutar
+         *
+         * @var array
+         */
         private $statusTexts = [
-           100 => 'Continue',
-           101 => 'Switching Protocols',
-           102 => 'Processing',            // RFC2518
-           200 => 'OK',
-           201 => 'Created',
-           202 => 'Accepted',
-           203 => 'Non-Authoritative Information',
-           204 => 'No Content',
-           205 => 'Reset Content',
-           206 => 'Partial Content',
-           207 => 'Multi-Status',          // RFC4918
-           208 => 'Already Reported',      // RFC5842
-           226 => 'IM Used',               // RFC3229
-           300 => 'Multiple Choices',
-           301 => 'Moved Permanently',
-           302 => 'Found',
-           303 => 'See Other',
-           304 => 'Not Modified',
-           305 => 'Use Proxy',
-           306 => 'Reserved',
-           307 => 'Temporary Redirect',
-           308 => 'Permanent Redirect',    // RFC7238
-           400 => 'Bad Request',
-           401 => 'Unauthorized',
-           402 => 'Payment Required',
-           403 => 'Forbidden',
-           404 => 'Not Found',
-           405 => 'Method Not Allowed',
-           406 => 'Not Acceptable',
-           407 => 'Proxy Authentication Required',
-           408 => 'Request Timeout',
-           409 => 'Conflict',
-           410 => 'Gone',
-           411 => 'Length Required',
-           412 => 'Precondition Failed',
-           413 => 'Request Entity Too Large',
-           414 => 'Request-URI Too Long',
-           415 => 'Unsupported Media Type',
-           416 => 'Requested Range Not Satisfiable',
-           417 => 'Expectation Failed',
-           418 => 'I\'m a teapot',                                               // RFC2324
-           422 => 'Unprocessable Entity',                                        // RFC4918
-           423 => 'Locked',                                                      // RFC4918
-           424 => 'Failed Dependency',                                           // RFC4918
-           425 => 'Reserved for WebDAV advanced collections expired proposal',   // RFC2817
-           426 => 'Upgrade Required',                                            // RFC2817
-           428 => 'Precondition Required',                                       // RFC6585
-           429 => 'Too Many Requests',                                           // RFC6585
-           431 => 'Request Header Fields Too Large',                             // RFC6585
-           500 => 'Internal Server Errors',
-           501 => 'Not Implemented',
-           502 => 'Bad Gateway',
-           503 => 'Service Unavailable',
-           504 => 'Gateway Timeout',
-           505 => 'HTTP Version Not Supported',
-           506 => 'Variant Also Negotiates (Experimental)',                      // RFC2295
-           507 => 'Insufficient Storage',                                        // RFC4918
-           508 => 'Loop Detected',                                               // RFC5842
-           510 => 'Not Extended',                                                // RFC2774
-           511 => 'Network Authentication Required',                             // RFC6585
+            100 => 'Continue',
+            101 => 'Switching Protocols',
+            102 => 'Processing',            // RFC2518
+            200 => 'OK',
+            201 => 'Created',
+            202 => 'Accepted',
+            203 => 'Non-Authoritative Information',
+            204 => 'No Content',
+            205 => 'Reset Content',
+            206 => 'Partial Content',
+            207 => 'Multi-Status',          // RFC4918
+            208 => 'Already Reported',      // RFC5842
+            226 => 'IM Used',               // RFC3229
+            300 => 'Multiple Choices',
+            301 => 'Moved Permanently',
+            302 => 'Found',
+            303 => 'See Other',
+            304 => 'Not Modified',
+            305 => 'Use Proxy',
+            306 => 'Reserved',
+            307 => 'Temporary Redirect',
+            308 => 'Permanent Redirect',    // RFC7238
+            400 => 'Bad Request',
+            401 => 'Unauthorized',
+            402 => 'Payment Required',
+            403 => 'Forbidden',
+            404 => 'Not Found',
+            405 => 'Method Not Allowed',
+            406 => 'Not Acceptable',
+            407 => 'Proxy Authentication Required',
+            408 => 'Request Timeout',
+            409 => 'Conflict',
+            410 => 'Gone',
+            411 => 'Length Required',
+            412 => 'Precondition Failed',
+            413 => 'Request Entity Too Large',
+            414 => 'Request-URI Too Long',
+            415 => 'Unsupported Media Type',
+            416 => 'Requested Range Not Satisfiable',
+            417 => 'Expectation Failed',
+            418 => 'I\'m a teapot',                                               // RFC2324
+            422 => 'Unprocessable Entity',                                        // RFC4918
+            423 => 'Locked',                                                      // RFC4918
+            424 => 'Failed Dependency',                                           // RFC4918
+            425 => 'Reserved for WebDAV advanced collections expired proposal',   // RFC2817
+            426 => 'Upgrade Required',                                            // RFC2817
+            428 => 'Precondition Required',                                       // RFC6585
+            429 => 'Too Many Requests',                                           // RFC6585
+            431 => 'Request Header Fields Too Large',                             // RFC6585
+            500 => 'Internal Server Errors',
+            501 => 'Not Implemented',
+            502 => 'Bad Gateway',
+            503 => 'Service Unavailable',
+            504 => 'Gateway Timeout',
+            505 => 'HTTP Version Not Supported',
+            506 => 'Variant Also Negotiates (Experimental)',                      // RFC2295
+            507 => 'Insufficient Storage',                                        // RFC4918
+            508 => 'Loop Detected',                                               // RFC5842
+            510 => 'Not Extended',                                                // RFC2774
+            511 => 'Network Authentication Required',                             // RFC6585
         ];
         /**
          * İçeriği tutar
@@ -89,78 +109,37 @@
          * @var null|string
          */
         private $content = null;
-
         /**
          * Durum Kodunu tutar
          *
          * @var int
          */
         private $statusCode = 200;
-        private $AnonymFrameworkHeaders = [
 
-           'AnonymFrameworkVersion'     => FRAMEWORK_VERSION,
-           'AnonymFrameworkProjectName' => FRAMEWORK_NAME
-
-        ];
-
-
-        private $standartHeaders = [
-            'Content-Language' => 'en'
-        ];
-
-        private $headersBag;
-        private $cookie;
 
         /**
-         * Sınıfı başlatır.
+         * Başlık bilgilerini tutar
          *
-         * @param string $content
-         * @param int    $statusCode
+         * @var array
          */
-        public function __construct($content = '', $statusCode = 200)
-        {
+        private $headers;
+
+        /**
+         * Cookie başlıklarını tutar
+         *
+         * @var
+         */
+        private $cookieHeaders;
+
+        /**
+         * Sınıfı başlatır
+         *
+         * @param string $content içerik kısmı
+         * @param string $statusCode gönderilecek durum kodu
+         */
+        public function __construct($content = '', $statusCode = ''){
             $this->setContent($content);
             $this->setStatusCode($statusCode);
-            $this->setProtocolVersion('1.1');
-            $this->headersBag = Singleton::make('Anonym\Components\HttpClient\Response\HeadersBag');
-            $this->headersBag->headers = array_merge($this->AnonymFrameworkHeaders, $this->headersBag->headers);
-            $this->headersBag->headers = array_merge($this->standartHeaders, $this->headersBag->headers);
-            $this->cookieBag = new Cookie();
-        }
-
-        /**
-         * Charset'i döndürür
-         *
-         * @return string
-         */
-        public function getCharset()
-        {
-
-            return $this->charset;
-        }
-
-
-        /**
-         * Cookileri döndürür
-         *
-         * @return array
-         */
-        public function getCookies()
-        {
-
-            return $this->cookie->cookies;
-        }
-
-        /**
-         * @param string $type
-         * @return $this
-         */
-        public function setContentType($type = '')
-        {
-
-            $this->contentType = $type;
-
-            return $this;
         }
 
         /**
@@ -168,237 +147,162 @@
          */
         public function getContentType()
         {
-
             return $this->contentType;
         }
 
         /**
-         * Charset ataması yapar
-         *
-         * @param string $charset
-         * @return $this
+         * @param string $contentType
+         * @return Response
          */
-
-        public function setCharset($charset = 'utf-8')
+        public function setContentType($contentType)
         {
+            $this->contentType = $contentType;
 
+            return $this;
+        }
+
+        /**
+         * @return string
+         */
+        public function getProtocolVersion()
+        {
+            return $this->protocolVersion;
+        }
+
+        /**
+         * @param string $protocolVersion
+         * @return Response
+         */
+        public function setProtocolVersion($protocolVersion)
+        {
+            $this->protocolVersion = $protocolVersion;
+
+            return $this;
+        }
+
+        /**
+         * @return string
+         */
+        public function getCharset()
+        {
+            return $this->charset;
+        }
+
+        /**
+         * @param string $charset
+         * @return Response
+         */
+        public function setCharset($charset)
+        {
             $this->charset = $charset;
 
             return $this;
         }
 
         /**
-         * Http Protocol'unun version'unu ayarlar
-         *
-         * @param string $version
-         * @return $this
+         * @return array
          */
-        public function setProtocolVersion($version = '')
+        public function getStatusTexts()
         {
+            return $this->statusTexts;
+        }
 
-            $this->protocolVersion = $version;
+        /**
+         * @param array $statusTexts
+         * @return Response
+         */
+        public function setStatusTexts($statusTexts)
+        {
+            $this->statusTexts = $statusTexts;
 
             return $this;
         }
 
         /**
-         * İçeriği tanımlar
-         *
-         * @param string $content
-         * @return $this
+         * @return null|string
          */
+        public function getContent()
+        {
+            return $this->content;
+        }
 
-        public function setContent($content = null)
+        /**
+         * @param null|string $content
+         * @return Response
+         */
+        public function setContent($content)
         {
             $this->content = $content;
+
             return $this;
         }
 
-
-        public function json($content = null, $statusCode = 200)
+        /**
+         * @return int
+         */
+        public function getStatusCode()
         {
-
-            return new JsonResponse($content, $statusCode);
+            return $this->statusCode;
         }
 
         /**
-         * Durum Kodunu tanımlar
+         * @param int $statusCode
+         * @return Response
+         */
+        public function setStatusCode($statusCode)
+        {
+            $this->statusCode = $statusCode;
+
+            return $this;
+        }
+
+        /**
+         * @return array
+         */
+        public function getHeaders()
+        {
+            return $this->headers;
+        }
+
+        /**
+         * @param array $headers
+         * @return Response
+         */
+        public function setHeaders($headers)
+        {
+            $this->headers = $headers;
+
+            return $this;
+        }
+
+        /**
+         * Header kodu ekler mesaja
          *
-         * @param int $code
+         * @param $name
+         * @param null $value
          * @return $this
          */
-        public function setStatusCode($code = 200)
-        {
+        public function header($name, $value = null){
 
-            $this->statusCode = $code;
+            if($value === null){
+                $this->headers[] = $name;
+            }else{
+                $this->headers[] = $this->headerString($name, $value);
+            }
 
             return $this;
         }
 
         /**
-         * Header kodunu oluşturur
+         * Header metnini oluşturur
          *
-         * @param $key
+         * @param $name
          * @param string $value
          * @return string
          */
-        private function generateHeaderString($key, $value = '')
-        {
-
+        private function headerString($name, $value = ''){
             return sprintf("%s: %s", settype($key, "string"), settype($key, "string"));
         }
 
-        /**
-         * Headerları atar
-         */
 
-        private function runHeaders()
-        {
-
-            header(
-               sprintf("Content-Type: %s; charset=%s", $this->getContentType(), $this->getCharset())
-            );
-            foreach ($this->headersBag->getHeaders() as $header => $value) {
-
-                header($this->generateHeaderString($header, $value));
-            }
-        }
-
-        /**
-         * Cookieleri kullanır
-         *
-         */
-        private function useCookies()
-        {
-
-            $cookies = $this->headersBag->getCookies();
-            foreach ($cookies as $cookie) {
-                $cookie = sprintf('Set-Cookie: %s', $cookie);
-                header($cookie);
-            }
-        }
-
-        /**
-         * Protocol version ve code atamasını yapar
-         */
-        private function setProtocolAndCode()
-        {
-
-            $code = $this->statusCode;
-            if (isset($this->statusTexts[$code])) {
-                $text = $this->statusTexts[$code];
-            } else {
-                $text = '';
-            }
-            header(sprintf("%s %d %s ", $this->protocolVersion, $code, $text));
-            http_response_code($code);
-        }
-
-        /**
-         * Başlıkları gönderir
-         */
-        private function sendHeaders(){
-
-            $this->setProtocolAndCode();
-            $this->useCookies();
-            $this->runHeaders();
-        }
-
-        /**
-         *  İçeriği gönderir
-         */
-        public function sendContent()
-        {
-
-            $content = (null !== $this->content) ? $this->content: Capture::getContent();
-            echo $content;
-
-        }
-        /**
-         * Çıktıyı Gönderiri
-         *
-         * @throws HttpResponseException
-         */
-
-        public function send()
-        {
-            if (!headers_sent()) {
-
-                $this->sendHeaders();
-                $this->sendContent();
-
-            } else {
-
-                throw new HttpResponseException(
-                   'Başlıklar  zaten gönderilimiş, bu işlem gerçekleştirilemez'
-                );
-            }
-        }
-
-        /**
-         * Sayfayı 404 yapar.
-         *
-         * @return $this
-         */
-        public function setPage404()
-        {
-
-            $this->statusCode = 404;
-            return $this;
-        }
-
-        /**
-         * yeni bir view objesi döndürür
-         *
-         * @param       $fileName
-         * @param array $params
-         * @return $this
-         */
-        public function view($fileName, $params = [])
-        {
-
-            return View::make($fileName, $params);
-        }
-
-        /**
-         * Twig objesi döndürür
-         *
-         * @param       $fileName
-         * @param array $params
-         * @return $this
-         */
-        public function twig($fileName, $params = [])
-        {
-
-            return Twig::make($fileName, $params);
-        }
-
-        /**
-         * Yeni bir response objesi oluşturur
-         *
-         * @param string $content
-         * @param int    $statusCode
-         * @return static
-         */
-        public function make($content = '', $statusCode = 200)
-        {
-
-            return new static($content, $statusCode);
-        }
-
-        /**
-         * Dinamik olarak method çağrımı
-         *
-         * @param $name
-         * @param $params
-         * @return mixed
-         */
-        public function __call($name, $params)
-        {
-
-            if (is_callable([$this->headersBag, $name])) {
-
-                return call_user_func_array([$this->headersBag, $name], $params);
-            }
-        }
     }
