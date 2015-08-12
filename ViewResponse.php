8f807a13157fdd5a9534509ea1696d71d384911d
Vahit Şerif Sağlam <serif.saglam119@gmail.com>
2015-08-12 21:28:26 +0300

    added anonym-view require

---
+++ ViewResponse.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Bu Dosya AnonymFramework'e ait bir dosyadýr.
+ *
+ * @author vahitserifsaglam <vahit.serif119@gmail.com>
+ * @see http://gemframework.com
+ *
+ */
+
+
+namespace Anonym\Components\HttpClient;
+
+/**
+ * Class ViewResponse
+ * @package Anonym\Components\HttpClient
+ */
+class ViewResponse extends Response
+{
+
+}
