0c5e9b2ee29d7cb2b71ef9212a4b453adb2990a5
Vahit Şerif Sağlam <serif.saglam119@gmail.com>
2015-08-12 21:40:17 +0300

    updated  xml response class

--- XmlResponse.php
+++ XmlResponse.php
@@ -10,8 +10,32 @@
 
 namespace Anonym\Components\HttpClient;
 
-
-class XmlResponse
+/**
+ * Class XmlResponse
+ * @package Anonym\Components\HttpClient
+ */
+class XmlResponse extends Response implements ResponseInterface
 {
 
+    /**
+     * Sýnýfý baþlatýr ve içerik ve durum kodunu ayalar
+     *
+     * @param string $content Ýçerik
+     * @param int $statusCode Durum kodu
+     */
+    public function __construct($content = '', $statusCode = 200)
+    {
+        parent::__construct($content, $statusCode);
+        $this->setContentType('text/xml');
+    }
+
+    /**
+     *
+     * Ýçeriði gönderiri
+     *
+     */
+    public function send()
+    {
+        parent::send();
+    }
 }
