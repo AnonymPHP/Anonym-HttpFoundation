386bf03a3b058ccae48e13dc4419a06cd522c953
anonymframework <serif.saglam119@gmail.com>
2015-08-27 02:29:26 +0300

    fixed a little bug

--- RequestHeaders.php
+++ RequestHeaders.php
@@ -1,60 +1,60 @@
 <?php
-    /**
-     * Bu Dosya AnonymFramework'e ait bir dosyadır.
-     *
-     * @author vahitserifsaglam <vahit.serif119@gmail.com>
-     * @see http://gemframework.com
-     *
-     */
+/**
+ * Bu Dosya AnonymFramework'e ait bir dosyadır.
+ *
+ * @author vahitserifsaglam <vahit.serif119@gmail.com>
+ * @see http://gemframework.com
+ *
+ */
 
-    namespace Anonym\Components\HttpClient;
+namespace Anonym\Components\HttpClient;
+
+/**
+ * Class RequestHeaders
+ * @package Anonym\Components\HttpClient
+ */
+class RequestHeaders implements RequestHeadersInterface
+{
+    /**
+     * Headerları depolar
+     *
+     * @var array|false
+     */
+    protected $headers;
 
     /**
-     * Class RequestHeaders
-     * @package Anonym\Components\HttpClient
+     * server bilgilerini depolar
+     *
+     * @var array
      */
-    class RequestHeaders implements RequestHeadersInterface
+    private $server;
+
+    /**
+     * sınıfı başlatır
+     */
+    public function __construct()
     {
-        /**
-         * Headerları depolar
-         *
-         * @var array|false
-         */
-        protected $headers;
-
-        /**
-         * server bilgilerini depolar
-         *
-         * @var array
-         */
-        private $server;
-
-        /**
-         * sınıfı başlatır
-         */
-        public function __construct()
-        {
-            $this->headers = getallheaders();
-            $this->server = $_SERVER;
-        }
-
-        /**
-         * Server verilerini döndürür
-         *
-         * @return mixed
-         */
-        public function getServer()
-        {
-            return $this->server;
-        }
-
-        /**
-         * Header'ları ekler
-         *
-         * @return mixed
-         */
-        public function getHeaders()
-        {
-            return $this->headers;
-        }
+        $this->headers = function_exists('getallheaders') ? getallheaders() : [];
+        $this->server = $_SERVER;
     }
+
+    /**
+     * Server verilerini döndürür
+     *
+     * @return mixed
+     */
+    public function getServer()
+    {
+        return $this->server;
+    }
+
+    /**
+     * Header'ları ekler
+     *
+     * @return mixed
+     */
+    public function getHeaders()
+    {
+        return $this->headers;
+    }
+}
