a20d7776698bd471e2420db80e49d906dabd2490
anonymframework <serif.saglam119@outlook.com>
2015-09-06 15:46:19 +0300

    made some updates

--- Query.php
+++ Query.php
@@ -15,38 +15,38 @@ namespace Anonym\Components\HttpClient;
 class Query
 {
     /**
-     * $name' e atanan veriye göre $_POST da veri varmı yokmu onu kontrol eder
+     * $name' e atanan veriye göre $_GET da veri varmı yokmu onu kontrol eder
      *
      * @param string $name
      * @return boolean
      */
     public static function has($name = null)
     {
-        if ($_POST) {
-            return isset($_POST[$name]);
+        if (isset($_GET)) {
+            return isset($_GET[$name]);
         }
     }
 
     /**
-     * $name'in $_POST içinde var olup olmadığına bakmazsızın veriyi çağırır
+     * $name'in $_GET içinde var olup olmadığına bakmazsızın veriyi çağırır
      *
      * @param string $name
      * @return mixed
      */
     public static function get($name)
     {
-        return $_POST[$name];
+        return $_GET[$name];
     }
 
     /**
-     * $_POST içinde $name'e $value' i atar;
+     * $_GET içinde $name'e $value' i atar;
      *
      * @param string $name
      * @param mixed $value
      */
     public static function set($name, $value)
     {
-        $_POST[$name] = $value;
+        $_GET[$name] = $value;
     }
 
     /**
@@ -56,7 +56,7 @@ class Query
      */
     public static function delete($name)
     {
-        unset($_POST[$name]);
+        unset($_GET[$name]);
     }
 
     /**
@@ -66,8 +66,8 @@ class Query
      */
     public static function getAll()
     {
-        if (isset($_POST)) {
-            return $_POST;
+        if (isset($_GET)) {
+            return $_GET;
         } else {
             return false;
         }
