52d318686cc535d7e552a127ddbd8c8d87d19570
anonymframework <serif.saglam119@outlook.com>
2015-09-06 15:45:47 +0300

    added query class

---
+++ Query.php
@@ -0,0 +1,75 @@
+<?php
+/**
+ * This file belongs to the AnoynmFramework
+ *
+ * @author vahitserifsaglam <vahit.serif119@gmail.com>
+ * @see http://gemframework.com
+ *
+ * Thanks for using
+ */
+
+
+namespace Anonym\Components\HttpClient;
+
+
+class Query
+{
+    /**
+     * $name' e atanan veriye göre $_POST da veri varmı yokmu onu kontrol eder
+     *
+     * @param string $name
+     * @return boolean
+     */
+    public static function has($name = null)
+    {
+        if ($_POST) {
+            return isset($_POST[$name]);
+        }
+    }
+
+    /**
+     * $name'in $_POST içinde var olup olmadığına bakmazsızın veriyi çağırır
+     *
+     * @param string $name
+     * @return mixed
+     */
+    public static function get($name)
+    {
+        return $_POST[$name];
+    }
+
+    /**
+     * $_POST içinde $name'e $value' i atar;
+     *
+     * @param string $name
+     * @param mixed $value
+     */
+    public static function set($name, $value)
+    {
+        $_POST[$name] = $value;
+    }
+
+    /**
+     * $_GET içinden $name'in değerini siler
+     *
+     * @param string $name
+     */
+    public static function delete($name)
+    {
+        unset($_POST[$name]);
+    }
+
+    /**
+     * @return mixed
+     *
+     * Post verilerini döndürür
+     */
+    public static function getAll()
+    {
+        if (isset($_POST)) {
+            return $_POST;
+        } else {
+            return false;
+        }
+    }
+}
