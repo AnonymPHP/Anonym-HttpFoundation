4e009e12b2d0190458f26bcdeeb34d362210b39a
anonymframework <serif.saglam119@outlook.com>
2015-09-20 04:03:06 +0300

    made some updates

--- Input.php
+++ Input.php
@@ -21,7 +21,7 @@ class Input
      * @param string $name
      * @return boolean
      */
-    public static function has($name = null)
+    public  function has($name = null)
     {
         if (isset($_POST)) {
             return isset($_POST[$name]);
@@ -34,7 +34,7 @@ class Input
      * @param string $name
      * @return mixed
      */
-    public static function get($name)
+    public  function get($name)
     {
         return $_POST[$name];
     }
@@ -45,7 +45,7 @@ class Input
      * @param string $name
      * @param mixed $value
      */
-    public static function set($name, $value)
+    public  function set($name, $value)
     {
         $_POST[$name] = $value;
     }
@@ -55,7 +55,7 @@ class Input
      *
      * @param string $name
      */
-    public static function delete($name)
+    public  function delete($name)
     {
         unset($_POST[$name]);
     }
@@ -65,7 +65,7 @@ class Input
      *
      * Post verilerini döndürür
      */
-    public static function getAll()
+    public  function getAll()
     {
         if (isset($_POST)) {
             return $_POST;
